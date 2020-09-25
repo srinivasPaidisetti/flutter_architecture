4388a22954664486938864dd003d3eb1b81c8db1
srinivas <srinivas@droid5.com>
2020-09-25 20:29:25 +0530

    changed the files

---
+++ lib/service/util_services.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class UtilService {
+  static Future showDialogBox(
+      BuildContext context, String title, String message,
+      {List<Widget> actions}) {
+    return showDialog<Null>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Container(
+          width: 270.0,
+          child: new AlertDialog(
+            title: new Text(title),
+            content: new SingleChildScrollView(
+              child: new ListBody(
+                children: <Widget>[
+                  new Text(message),
+                ],
+              ),
+            ),
+            actions: actions ??
+                <Widget>[
+                  new FlatButton(
+                    child: new Text('OK'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+          ),
+        );
+      },
+    );
+  }
+
+  static void showSnackBar(scaffoldKey, message, {Duration duration}) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      duration: duration ?? Duration(milliseconds: 3000),
+    );
+    scaffoldKey.currentState.showSnackBar(snackBar);
+  }
+
+  static void navigateBack(BuildContext context, {result}) {
+    return Navigator.pop(context, result);
+  }
+
+  static Future navigateTo(BuildContext context, Widget screen) {
+    return Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => screen),
+    );
+  }
+
+
+  static Future navigateToAndPopAll(BuildContext context, Widget screen) {
+    return Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) => screen,
+        ),
+        (Route<dynamic> route) => false);
+  }
+}
--- lib/utils/validations.dart
+++ lib/utils/validations.dart
@@ -18,3 +18,21 @@ String validatePassword(String value) {
     return null;
   }
 }
+
+String validateName(String value) {
+  if (value.isEmpty && value.length == 0) {
+    return "Name can't be empty";
+  } else {
+    return null;
+  }
+}
+
+String validatePhoneNumber(String value) {
+  if (value.isEmpty && value.length == 0) {
+    return "Phone number can't be empty";
+  } else if (value.length < 9) {
+    return "Phone number is not correct format";
+  } else {
+    return null;
+  }
+}
